8f244339e997a589f15bed52e0fe37215e678b44
Fabio Kelvin <kelvinfabio2@gmail.com>
2025-09-24 10:41:09 +0100

    📕chore: ajustes adicionado a pagina do not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
 import { logo } from '@/src/constants/images';
+import { ROUTES } from '@/src/constants/routes';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const NotFound = () => {
@@ -8,7 +10,7 @@ const NotFound = () => {
       <Image src={logo} alt="Wundu Logo" className="mb-8 w-20 h-auto" />
       <h1 className="text-4xl font-bold text-gray-800">404 - Página Não Encontrada</h1>
       <p className="mt-4 text-lg text-gray-600">Desculpe, a página que você está procurando não existe ou não está disponível.</p>
-      <a href="/" className="mt-6 text-yellow-500 hover:underline">Voltar para a página inicial</a>
+      <Link href={ROUTES.HOME} className="mt-6 text-yellow-500 hover:underline">Voltar para a página inicial</Link>
     </div>
   );
 };
