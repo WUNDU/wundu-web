e3a43ae381450083425582529935b754f7d6a073
nill-pixel <nilvanysunguessungue@gmail.com>
2025-11-10 13:09:32 +0100

    📕chore: ajuste no logotype para a imagem ter qualidade

--- src/components/atoms/LogoType.tsx
+++ src/components/atoms/LogoType.tsx
@@ -1,10 +1,9 @@
 import { logotype } from "@/src/constants/images";
-import Image from "next/image";
 
 const LogoType = () => (
   <div className="flex items-center space-x-2">
     <div className="">
-      <Image src={logotype} alt={"logotype"} />
+      <img src="/assets/logotype.svg" alt="logotype" />
     </div>
   </div>
 );
