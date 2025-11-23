450c6bc9c2307cee8f2c75631e23f674d91d1cf6
nill-pixel <nilvanysunguessungue@gmail.com>
2025-11-23 15:43:10 +0100

    🐛 fix: correção do launch props

--- src/ui/templates/landing-page/ContactLandingPage/index.tsx
+++ src/ui/templates/landing-page/ContactLandingPage/index.tsx
@@ -5,11 +5,8 @@ import LandingFooter from "@/ui/organisms/LandingFooter";
 import ContactMethods from "@/ui/organisms/ContactMethods";
 import ContactFormSection from "@/ui/organisms/ContactFormSection";
 import { PageHero } from "@/ui/organisms";
-import { useLaunchStatus } from "@/hooks/useLaunchStatus";
 
 const ContactLandingPage: React.FC = () => {
-  const { isLaunched } = useLaunchStatus();
-
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
@@ -33,11 +30,10 @@ const ContactLandingPage: React.FC = () => {
     <div className="min-h-screen bg-white font-inter">
       {/* Header Space Reservation */}
       <div className="h-24 bg-white"></div>
-      <LandingHeader isLaunched={isLaunched} />
+      <LandingHeader />
       <PageHero
         title="Fale Connosco"
         description="Tem alguma dúvida ou sugestão? A nossa equipe está pronta para te ajudar."
-        isLaunched={isLaunched}
       />
 
       <ContactMethods />
